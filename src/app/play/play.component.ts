e6ac62c3d08fe90e0b2bf5d25bf1eebf0421d3d6
Ryan Krawczyk <rkrawczy@USMBP16rkrawczy.local>
2022-03-02 11:25:22 -0500

    Handle device rotation events

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -10,10 +10,12 @@ import { DeckService } from '../services/deck.service';
 export class PlayComponent implements OnDestroy, OnInit {
   currentCard: string = '';
   deckName: string = '';
+  endReached: boolean = false;
   previousScore: number | null = null;
   roundStarted: boolean = false;
-  // Default round length is 60 seconds
   remainingSec: number = 60;
+  resultCorrect: boolean = false;
+  resultSkip: boolean = false;
   score: number = 0;
   startPosition: number = 0;
 
@@ -42,18 +44,34 @@ export class PlayComponent implements OnDestroy, OnInit {
 
   // Handles changes in gamma rotation
   deviceOrientationHandler(e: DeviceOrientationEvent): void {
-    // TODO: handle changes in gamma rotation
-    if ('user cashed or skipped') {
-      if ('user cashed') {
+    // Correct guess: gamma rotation between 0 and 10 degrees
+    const correct = Boolean(e.gamma && e.gamma > 0 && e.gamma < 10);
+    // Skip: gamma rotation between 0 and -10 degrees
+    const skip = Boolean(e.gamma && e.gamma < 0 && e.gamma > -10);
+    if (correct || skip) {
+      this.displayResult(correct);
+      if (correct) {
         this.score++;
       }
       const nextCard = this.deckService.nextCard(this.startPosition);
       if (!nextCard) {
         this.end();
-        // TODO: handle end of deck being reached
+        this.endReached = true;
       }
       this.currentCard = this.deckService.getCurrentCard();
     }
+  }
+
+  // Pauses device orientation listener for 500 ms and indicates a correct guess or skip
+  displayResult(correct: boolean): void {
+    removeEventListener('deviceorientation', this.deviceOrientationHandler);
+    this.resultCorrect = correct;
+    this.resultSkip = !correct;
+    setTimeout(() => {
+      this.resultCorrect = false;
+      this.resultSkip = false;
+      addEventListener('deviceorientation', this.deviceOrientationHandler);
+    }, 500);
   }
 
   // Sets previous score and wristes updated deck
@@ -72,6 +90,7 @@ export class PlayComponent implements OnDestroy, OnInit {
   start(): void {
     this.roundStarted = true;
     if (this.deckService.currentDeck) {
+      // Default round length is 60 sec
       this.remainingSec = this.deckService.currentDeck.roundLengthSec;
     }
     // Decrement round length to count down seconds
